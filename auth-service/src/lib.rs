4a5afdfe598245b59d02158980a8a4d308ae9b13
Viktor Zahorodnii <viktor.zahorodnii@railsware.com>
2025-10-08 15:08:48 +0300

    [Spint 1][Task 6] feat: implement dummy 200 status code endpoints for auth service

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 
-use axum::{serve::Serve, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
 use tower_http::services::ServeDir;
 
 // This struct encapsulates our application-related logic.
@@ -13,7 +13,13 @@ pub struct Application {
 
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
-        let router = Router::new().nest_service("/", ServeDir::new("assets"));
+        let router = Router::new()
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(Self::signup))
+            .route("/login", post(Self::login))
+            .route("/verify-2fa", post(Self::verify_2fa))
+            .route("/logout", post(Self::logout))
+            .route("/verify-token", post(Self::verify_token));
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
@@ -25,5 +31,25 @@ impl Application {
     pub async fn run(self) -> Result<(), std::io::Error> {
         println!("listening on {}", &self.address);
         self.server.await
+    }
+
+    async fn signup() -> impl IntoResponse {
+        StatusCode::OK.into_response()
+    }
+
+    async fn login() -> impl IntoResponse {
+        StatusCode::OK.into_response()
+    }
+
+    async fn verify_2fa() -> impl IntoResponse {
+        StatusCode::OK.into_response()
+    }
+
+    async fn logout() -> impl IntoResponse {
+        StatusCode::OK.into_response()
+    }
+
+    async fn verify_token() -> impl IntoResponse {
+        StatusCode::OK.into_response()
     }
 }
