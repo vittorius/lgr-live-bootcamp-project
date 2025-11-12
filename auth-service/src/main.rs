4bc70fff0beca93fa244c5dfcc34aa6a2516e21c
Viktor Zahorodnii <viktor.zahorodnii@railsware.com>
2025-11-12 21:49:48 +0200

    [Sprint 6][Task 5] feat: add Postmark client and tests

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,13 +1,15 @@
 use std::sync::Arc;
 
 use auth_service::app_state::AppState;
+use auth_service::domain::Email;
 use auth_service::services::data_stores::{
     PostgresUserStore, RedisBannedTokenStore, RedisTwoFACodeStore,
 };
-use auth_service::services::MockEmailClient;
-use auth_service::utils::constants::{prod, DATABASE_URL, REDIS_HOST_NAME};
+use auth_service::services::postmark_email_client::PostmarkEmailClient;
+use auth_service::utils::constants::{DATABASE_URL, POSTMARK_AUTH_TOKEN, REDIS_HOST_NAME, prod};
 use auth_service::utils::tracing::init_tracing;
 use auth_service::{get_postgres_pool, get_redis_client, Application};
+use reqwest::Client;
 use sqlx::PgPool;
 use tokio::sync::RwLock;
 
@@ -24,7 +26,7 @@ async fn main() {
         redis_connection.clone(),
     )));
     let two_fa_code_store = Arc::new(RwLock::new(RedisTwoFACodeStore::new(redis_connection)));
-    let email_client = Arc::new(MockEmailClient {});
+    let email_client = Arc::new(configure_postmark_email_client());
 
     let app_state = AppState::new(
         user_store,
@@ -60,4 +62,18 @@ fn configure_redis() -> redis::Connection {
         .expect("Failed to get Redis client")
         .get_connection()
         .expect("Failed to get Redis connection")
+}
+
+fn configure_postmark_email_client() -> PostmarkEmailClient {
+    let http_client = Client::builder()
+        .timeout(prod::email_client::TIMEOUT)
+        .build()
+        .expect("Failed to build HTTP client");
+
+    PostmarkEmailClient::new(
+        prod::email_client::BASE_URL.to_owned(),
+        Email::parse(prod::email_client::SENDER.to_owned().into()).unwrap(),
+        POSTMARK_AUTH_TOKEN.to_owned(),
+        http_client,
+    )
 }
--- auth-service/src/services.rs
+++ auth-service/src/services.rs
@@ -1,4 +1,3 @@
 pub mod data_stores;
-mod mock_email_client;
-
-pub use mock_email_client::*;
+pub mod mock_email_client;
+pub mod postmark_email_client;
